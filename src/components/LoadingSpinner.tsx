3b76a1ca09818852b8663acdbb891d1fa4c9a9a0
ufukguler <ufukguler@users.noreply.github.com>
2025-04-19 21:51:36 +0200

    fix loading spinner css

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -20,7 +20,7 @@ export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({isLoading}) => {
         ref={nodeRef}
         className="position-fixed top-0 start-0 end-0 bottom-0 d-flex align-items-center justify-content-center bg-dark bg-opacity-50"
       >
-        <div className="spinner-border text-primary loading-spinner" role="status">
+        <div className="spinner-border text-primary" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
