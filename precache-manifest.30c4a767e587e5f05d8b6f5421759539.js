df16d293aa4721b8f06a327751035bbbb600dcaf
ufukguler <ufukguler@users.noreply.github.com>
2025-04-30 19:56:51 +0000

    deploy: de79c270b5b29fc412a8c3a2a7656a6caa60c85d

---
+++ precache-manifest.30c4a767e587e5f05d8b6f5421759539.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be0a194fb6d793b6001aaa8f90c9e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7bcb22576b377b9821a",
+    "url": "/static/css/2.466e3e6d.chunk.css"
+  },
+  {
+    "revision": "33bbe51c6297946e686e",
+    "url": "/static/css/main.df2bbec3.chunk.css"
+  },
+  {
+    "revision": "e7bcb22576b377b9821a",
+    "url": "/static/js/2.50da9326.chunk.js"
+  },
+  {
+    "revision": "33bbe51c6297946e686e",
+    "url": "/static/js/main.299e742e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba49e844892321d8540ea3b7c088cf97",
+    "url": "/static/media/bootstrap-icons.ba49e844.woff"
+  },
+  {
+    "revision": "cc1e5eda776be5f0ff614285c31d4892",
+    "url": "/static/media/bootstrap-icons.cc1e5eda.woff2"
+  }
+]);
